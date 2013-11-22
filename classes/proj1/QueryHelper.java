6ac6f7c5825eadfa7d57832de9c4739501b73a49
jbonot <jbonot@ualberta.ca>
2013-11-22 14:53:39 -0700

    Fixed query so that admin sees all photos.  Added admin user into examples.

--- classes/proj1/QueryHelper.java
+++ classes/proj1/QueryHelper.java
@@ -9,9 +9,10 @@ import java.sql.Date;
  * A query helper.
  */
 public class QueryHelper {
-	private static String FETCH_THUMBNAILS = "SELECT photo_id, subject "
-			+ "FROM images, group_lists "
-			+ "where ((permitted=2 and owner_name=?) "
+	private static String ADMIN = "admin";
+	private static String FETCH_USER_ADMIN = "SELECT photo_id, subject from images ";
+	private static String FETCH_USER_THUMBNAILS = "SELECT photo_id, subject "
+			+ "FROM images, group_lists where ((permitted=2 and owner_name=?) "
 			+ "or (group_id=permitted and friend_id=?)) ";
 	private SQLAdapter adapter;
 	private String user;
@@ -22,31 +23,43 @@ public class QueryHelper {
 	}
 
 	public ResultSet getHomeItems() {
-		PreparedStatement stmt = adapter.prepareStatement(FETCH_THUMBNAILS);
-		try {
-			stmt.setString(1, user);
-			stmt.setString(2, user);
-			return adapter.executeQuery(stmt);
-		} catch (SQLException e) {
-			e.printStackTrace();
+		if (this.user.equals(ADMIN)) {
+
+			return adapter.executeFetch(FETCH_USER_ADMIN);
+
+		} else {
+
+			PreparedStatement stmt = adapter
+					.prepareStatement(FETCH_USER_THUMBNAILS);
+			try {
+				stmt.setString(1, user);
+				stmt.setString(2, user);
+				return adapter.executeQuery(stmt);
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
+			}
 		}
-		return null;
 	}
 
 	public ResultSet getSearchItems(String query, Date dateStart, Date dateEnd) {
-		StringBuilder queryString = new StringBuilder(FETCH_THUMBNAILS);
+		StringBuilder queryString = new StringBuilder(this.user.equals(ADMIN) ? FETCH_USER_ADMIN : FETCH_USER_THUMBNAILS);
+
 		int i = 1;
-		
+		String conjunction = this.user.equals(ADMIN) ? "where " : "and ";
+
 		if (dateStart != null) {
-			queryString.append("and timing >= ? ");
+			queryString.append(conjunction + "timing >= ? ");
+			conjunction = "and ";
 		}
 
 		if (dateEnd != null) {
-			queryString.append("and timing <= ? ");
+			queryString.append(conjunction + "timing <= ? ");
+			conjunction = "and ";
 		}
 
 		if (query != null) {
-			queryString.append("and (contains(subject, ?, 6) > 0 ");
+			queryString.append(conjunction + "(contains(subject, ?, 6) > 0 ");
 			queryString.append("or contains(place, ?, 3) > 0 ");
 			queryString.append("or contains(description, ?, 1) > 0) ");
 			queryString
@@ -59,8 +72,10 @@ public class QueryHelper {
 				.toString());
 
 		try {
-			doSearch.setString(i++, user);
-			doSearch.setString(i++, user);
+			if (!this.user.equals(ADMIN)) {
+				doSearch.setString(i++, user);
+				doSearch.setString(i++, user);
+			}
 
 			if (dateStart != null) {
 				doSearch.setDate(i++, dateStart);
@@ -77,11 +92,11 @@ public class QueryHelper {
 			}
 
 			return adapter.executeQuery(doSearch);
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
 }
